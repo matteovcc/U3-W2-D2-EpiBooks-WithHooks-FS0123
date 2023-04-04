0d38ba17c0b78cc0caa97f33e1e8f03a6d203bb3
matteovcc <matteovcc99@gmail.com>
2023-04-04 15:59:28 +0200

    code formatted

--- src/components/AddComment.jsx
+++ src/components/AddComment.jsx
@@ -1,155 +1,50 @@
-// import { Component } from 'react'
-// import { Button, Form } from 'react-bootstrap'
-
-// class AddComment extends Component {
-//   // state = {
-//   //   comment: {
-//   //     comment: '',
-//   //     rate: 1,
-//   //     elementId: this.props.asin,
-//   //   },
-//   // }
-
-//   componentDidUpdate(prevProps) {
-//     if (prevProps.asin !== this.props.asin) {
-//       this.setState({
-//         comment: {
-//           ...this.state.comment,
-//           elementId: this.props.asin,
-//         },
-//       })
-//     }
-//   }
-
-//   sendComment = async (e) => {
-//     e.preventDefault()
-//     try {
-//       let response = await fetch(
-//         'https://striveschool-api.herokuapp.com/api/comments',
-//         {
-//           method: 'POST',
-//           body: JSON.stringify(this.state.comment),
-//           headers: {
-//             'Content-type': 'application/json',
-//             Authorization: 'Bearer your-auth-token-goes-here',
-//           },
-//         }
-//       )
-//       if (response.ok) {
-//         alert('Comment was sent!')
-//         this.setState({
-//           comment: {
-//             comment: '',
-//             rate: 1,
-//             elementId: this.props.asin,
-//           },
-//         })
-//       } else {
-//         console.log('error')
-//         alert('something went wrong')
-//       }
-//     } catch (error) {
-//       console.log('error')
-//     }
-//   }
-
-//   render() {
-//     return (
-//       <div className="my-3">
-//         <Form onSubmit={this.sendComment}>
-//           <Form.Group>
-//             <Form.Label>Comment text</Form.Label>
-//             <Form.Control
-//               type="text"
-//               placeholder="Add comment here"
-//               value={this.state.comment.comment}
-//               onChange={(e) =>
-//                 this.setState({
-//                   comment: {
-//                     ...this.state.comment,
-//                     comment: e.target.value,
-//                   },
-//                 })
-//               }
-//             />
-//           </Form.Group>
-//           <Form.Group>
-//             <Form.Label>Rating</Form.Label>
-//             <Form.Control
-//               as="select"
-//               value={this.state.comment.rate}
-//               onChange={(e) =>
-//                 this.setState({
-//                   comment: {
-//                     ...this.state.comment,
-//                     rate: e.target.value,
-//                   },
-//                 })
-//               }
-//             >
-//               <option>1</option>
-//               <option>2</option>
-//               <option>3</option>
-//               <option>4</option>
-//               <option>5</option>
-//             </Form.Control>
-//           </Form.Group>
-//           <Button variant="primary" type="submit">
-//             Submit
-//           </Button>
-//         </Form>
-//       </div>
-//     )
-//   }
-// }
-
-// export default AddComment
-import { useState, useEffect } from 'react'
-import { Button, Form } from 'react-bootstrap'
+import { useState, useEffect } from "react";
+import { Button, Form } from "react-bootstrap";
 
 const AddComment = ({ asin }) => {
   const [comment, setComment] = useState({
-    comment: '',
+    comment: "",
     rate: 1,
     elementId: asin,
-  })
+  });
 
   useEffect(() => {
     setComment((prevComment) => ({
       ...prevComment,
       elementId: asin,
-    }))
-  }, [asin])
+    }));
+  }, [asin]);
 
   const sendComment = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     try {
       let response = await fetch(
-        'https://striveschool-api.herokuapp.com/api/comments',
+        "https://striveschool-api.herokuapp.com/api/comments",
         {
-          method: 'POST',
+          method: "POST",
           body: JSON.stringify(comment),
           headers: {
-            'Content-type': 'application/json',
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDJhYmYyMjE1ZjQzMjAwMTRhNzg2Y2EiLCJpYXQiOjE2ODA1MjMwNDMsImV4cCI6MTY4MTczMjY0M30.xwW-DuuSEs04i-3m7edBFIhcf8wnJJ1zrt6Pdr-w1p8',
+            "Content-type": "application/json",
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDJhYmYyMjE1ZjQzMjAwMTRhNzg2Y2EiLCJpYXQiOjE2ODA1MjMwNDMsImV4cCI6MTY4MTczMjY0M30.xwW-DuuSEs04i-3m7edBFIhcf8wnJJ1zrt6Pdr-w1p8",
           },
         }
-      )
+      );
       if (response.ok) {
-        alert('Comment was sent!')
+        alert("Comment was sent!");
         setComment({
-          comment: '',
+          comment: "",
           rate: 1,
           elementId: asin,
-        })
+        });
       } else {
-        console.log('error')
-        alert('something went wrong')
+        console.log("error");
+        alert("something went wrong");
       }
     } catch (error) {
-      console.log('error')
+      console.log("error");
     }
-  }
+  };
 
   return (
     <div className="my-3">
@@ -192,7 +87,7 @@ const AddComment = ({ asin }) => {
         </Button>
       </Form>
     </div>
-  )
-}
+  );
+};
 
-export default AddComment
+export default AddComment;
--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,39 +1,34 @@
-// state = {
-  //   selected: false,
-  // }
-  // onClick={() => this.setState({ selected: !this.state.selected })}
-  
-  // import CommentArea from './CommentArea'
-  import { useState } from "react";
-  import { Card } from "react-bootstrap";
-  
-  const SingleBook = (props) => {
-    const [selectedBook, setSelectedBook] = useState("");
-  
-    const changeSelectedBookHandler = () => {
-      setSelectedBook(props.book.asin);
-      props.changeSelectedBook(props.book.asin);
-    };
-    //qua sopra diamo a selectedBook un nuovo asin ogni volta che cliccheremo un nuovo book 
-  
-    return (
-      <>
-        <Card
-          onClick={changeSelectedBookHandler}
-          //se la card ha lo stesso asin del selectedBook allora si colora rosso , sennò grigio 
-          style={{
-            border: props.selectedBook === props.book.asin ? "3px solid red" : "3px solid #ebebeb",
-          }}
-        >
-          <Card.Img variant="top" src={props.book.img} />
-          <Card.Body>
-            <Card.Title style={{ color: "black" }}>
-              {props.book.title}
-            </Card.Title>
-          </Card.Body>
-        </Card>
-      </>
-    );
+import { useState } from "react";
+import { Card } from "react-bootstrap";
+
+const SingleBook = (props) => {
+  const [selectedBook, setSelectedBook] = useState("");
+
+  const changeSelectedBookHandler = () => {
+    setSelectedBook(props.book.asin);
+    props.changeSelectedBook(props.book.asin);
   };
-  
-  export default SingleBook;
+  //qua sopra diamo a selectedBook un nuovo asin ogni volta che cliccheremo un nuovo book
+
+  return (
+    <>
+      <Card
+        onClick={changeSelectedBookHandler}
+        //se la card ha lo stesso asin del selectedBook allora si colora rosso , sennò grigio
+        style={{
+          border:
+            props.selectedBook === props.book.asin
+              ? "3px solid red"
+              : "3px solid #ebebeb",
+        }}
+      >
+        <Card.Img variant="top" src={props.book.img} />
+        <Card.Body>
+          <Card.Title style={{ color: "black" }}>{props.book.title}</Card.Title>
+        </Card.Body>
+      </Card>
+    </>
+  );
+};
+
+export default SingleBook;
